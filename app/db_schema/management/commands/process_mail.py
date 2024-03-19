479c62d2bb6859df7fab8921c80c86376741771d
Senior Fullstack Developer <akira.murayama.dev@gmail.com>
2024-03-19 11:29:31 +0900

    fix mail cron

---
+++ app/db_schema/management/commands/process_mail.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db import *
+from django.db.models import *
+from django_mailbox.models import Message, Mailbox, MessageAttachment
+from db_schema.models import *
+import json
+
+
+
+class Command(BaseCommand):
+    help = "Closes the specified poll for voting"
+
+
+    def handle(self, *args, **options):
+        
+        m_messages = Message.objects.filter(Q(from_header__contains='<')|Q(to_header__contains='<'))
+
+        for m_message in m_messages:
+            m_message.from_header = m_message.from_address
+            m_message.to_header = m_message.to_addresses
+            m_message.save()
