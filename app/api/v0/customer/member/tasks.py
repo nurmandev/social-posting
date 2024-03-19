50ca30dbf31b3d3a25772af07643c65fcc4d99fe
Senior Fullstack Developer <akira.murayama.dev@gmail.com>
2024-03-20 03:01:01 +0900

    complete mail attachment

--- app/api/v0/customer/member/tasks.py
+++ app/api/v0/customer/member/tasks.py
@@ -21,11 +21,11 @@ def send_email_task(recepients, subject, body, attachment):
     )
     email_obj.content_subtype = "html"
 
-    print(attachment)
     for attach_id in attachment:
         m_attach = MessageAttachment.objects.get(id = attach_id)
-        print(m_attach.document.file.name)
-        email_obj.attach(m_attach.document.file.name, m_attach.document.file.read(), m_attach.document.file.content_type)
+        m_attach_serializer = MessageAttachmentSerializer(m_attach).data
+        
+        email_obj.attach(m_attach_serializer["info"]["name"], m_attach.document.file.read(), m_attach_serializer["info"]["content_type"])
 
     email_obj.send()
 
